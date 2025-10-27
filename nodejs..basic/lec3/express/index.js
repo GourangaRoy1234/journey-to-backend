d35b8897c9fbd0c7f07284f52511c0ae0f9458a5
GourangaRoy1234 <mechgouranga1234@gmail.com>
2025-10-27 04:22:11 +0000

    express done basic

--- nodejs..basic/lec3/express/index.js
+++ nodejs..basic/lec3/express/index.js
@@ -1,13 +1,29 @@
+//we discuss first express framework, then server creation, routing, middleware, error handling
 const express= require('express');
 //import express from 'express'
 
 const app = express();
+//middleware-> its a function that runs between request and response
+//why need middileware-> for logging, authentication, error handling , etc
+app.use("/", function(req, res, next){
+    console.log("this is middleware");
+    next();
+});
 //routes creation
 //app.get(route, callback(function (req, res) {}))
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
-app.get('/about', (req, res) => {
-  res.send('This is about page');
+app.get('/about', (req, res, next) => {
+  //res.send('This is about page');
+  return next(new Error('This is a forced error.'));
 })  
-app.listen(3000)
+//error handling middleware...always put at last
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+//server listening
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
